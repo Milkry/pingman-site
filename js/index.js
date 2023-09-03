71534f511c5c05e08fa5e8774c65aa737738faac
Panagiotis Stathopoulos <panosthathop@gmail.com>
2023-09-03 21:46:17 +0100

    - Added a new section to the site - Added slide right animations

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+function onLoad() {
+    window.addEventListener("scroll", reveal);
+    // Initial scroll position
+    reveal();
+}
+
 function scrollToView(elementId) {
     document.getElementById(elementId).scrollIntoView({
         behavior: 'smooth',
@@ -10,4 +16,18 @@ function scrollToTop() {
         top: 0,
         behavior: 'smooth'
     });
+}
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal:not(.active)");
+    var elementVisible = 30;
+
+    for (const element of reveals) {
+        var windowHeight = window.innerHeight;
+        var elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - elementVisible) {
+            element.classList.add("active");
+        }
+    }
 }
